5f761bd7bd3bcc1f86d2640feb0064cab4311a35
Annie Walsh <annie@anyonething.co.uk>
2021-04-20 21:16:04 +0100

    logic done for task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,43 @@
+class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+end
